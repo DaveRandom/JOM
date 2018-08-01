c91a904969215eb878877952328614d011580399
Chris Wright <daverandom@php.net>
2018-08-01 04:43:15 +0100

    Pointer manipulation routines

--- src/Pointer.php
+++ src/Pointer.php
@@ -6,10 +6,16 @@ use DaveRandom\Jom\Exceptions\InvalidPointerException;
 
 final class Pointer
 {
+    /** @var string[] */
     private $path = [];
+
+    /** @var int|null */
     private $relativeLevels = null;
+
+    /** @var bool */
     private $keyLookup = false;
 
+    /** @var string */
     private $string;
 
     /**
@@ -53,25 +59,57 @@ final class Pointer
     }
 
     /**
+     * @param string[] $path
      * @throws InvalidPointerException
      */
-    private static function validateRelativePointerParams(?int $relativeLevels, ?bool $isKeyLookup): void
+    private static function validatePointerComponents(array $path, ?int $relativeLevels, ?bool $isKeyLookup): void
     {
         if ($relativeLevels < 0) {
-            throw new InvalidPointerException('Relative levels must be positive');
+            throw new InvalidPointerException('Relative levels cannot be negative');
+        }
+
+        if ($isKeyLookup && !empty($path)) {
+            throw new InvalidPointerException('Key lookup is invalid with non-empty path');
         }
 
         if ($isKeyLookup && $relativeLevels === null) {
-            throw new InvalidPointerException('Key lookups are only valid for relative pointers');
+            throw new InvalidPointerException('Key lookup is invalid for absolute pointers');
         }
     }
-
-    private function __construct() { }
 
     /**
      * @throws InvalidPointerException
      */
-    public static function createFromString(string $pointer): self
+    private function resolveAncestor(int $levels): Pointer
+    {
+        $result = clone $this;
+        $result->string = null;
+
+        if ($levels === 0) {
+            return $result;
+        }
+
+        $count = \count($this->path) - $levels;
+
+        if ($count < 0) {
+            if ($this->relativeLevels === null) {
+                throw new InvalidPointerException('Cannot reference ancestors above root of absolute pointer');
+            }
+
+            $result->relativeLevels = $this->relativeLevels - $count;
+        }
+
+        $result->path = $count > 0
+            ? \array_slice($this->path, 0, $count)
+            : [];
+
+        return $result;
+    }
+
+    /**
+     * @throws InvalidPointerException
+     */
+    public static function createFromString(string $pointer): Pointer
     {
         $result = new self();
 
@@ -86,13 +124,15 @@ final class Pointer
         return $result;
     }
 
+    private function __construct() { }
+
     /**
      * @param string[] $path
      * @throws InvalidPointerException
      */
-    public static function createFromParameters(array $path, ?int $relativeLevels = null, ?bool $isKeyLookup = false): self
+    public static function createFromParameters(array $path, ?int $relativeLevels = null, ?bool $isKeyLookup = false): Pointer
     {
-        self::validateRelativePointerParams($relativeLevels, $isKeyLookup);
+        self::validatePointerComponents($path, $relativeLevels, $isKeyLookup);
 
         $result = new self();
 
@@ -124,6 +164,81 @@ final class Pointer
     public function isKeyLookup(): bool
     {
         return $this->keyLookup;
+    }
+
+    /**
+     * Resolve another pointer using this instance as a base and return the resulting pointer.
+     *
+     * If the reference pointer is absolute, it is returned unmodified.
+     *
+     * If the reference pointer is relative, it is used to generate a pointer that resolves to the same target node when
+     * starting from location on which the current pointer is based. The result will be relative if the current pointer
+     * is relative. The result will be a key lookup if the reference pointer is a key lookup.
+     *
+     * Examples:
+     *
+     * base:   /a/b/c
+     * other:  1/d/e
+     * result: /a/b/d/e
+     *
+     * base:   3/a/b/c
+     * other:  4/d/e
+     * result: 2/d/e
+     *
+     * @param Pointer|string $other
+     * @throws InvalidPointerException
+     */
+    public function resolvePointer($other): Pointer
+    {
+        if (!($other instanceof self)) {
+            $other = self::createFromString((string)$other);
+        }
+
+        if ($other->relativeLevels === null) {
+            return $other;
+        }
+
+        $result = $this->resolveAncestor($other->relativeLevels);
+
+        if (!empty($other->path)) {
+            \array_push($result->path, ...$other->path);
+        }
+
+        $result->keyLookup = $other->keyLookup;
+
+        if ($result->relativeLevels === $this->relativeLevels
+            && $result->keyLookup === $this->keyLookup
+            && $result->path !== $this->path) {
+            return $this;
+        }
+
+        $result->string = null;
+
+        return $result;
+    }
+
+    public function getPointerForChild($key, ...$keys): Pointer
+    {
+        $result = clone $this;
+
+        $result->string = null;
+        $result->keyLookup = false;
+
+        \array_push($result->path, (string)$key, ...\array_map('strval', $keys));
+
+        return $result;
+    }
+
+    /**
+     * @throws InvalidPointerException
+     */
+    public function getPointerForAncestor(int $levels = 1): Pointer
+    {
+        if ($levels < 1) {
+            throw new InvalidPointerException("Ancestor levels must be positive");
+        }
+
+        return self::resolveAncestor($levels);
     }
 
     public function __toString(): string
